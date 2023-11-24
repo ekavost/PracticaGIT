b2440330890cd452e8c228517a4a4794e9f4b26d
Ekaterina Vostrikova <ekavos@hotmail.com>
2023-11-24 17:11:48 +0100

    Primera versión del código - Con errores lógicos

--- PracticaGIT_EV2324/PracticaGIT_EV2324/Form1.Designer.cs
+++ PracticaGIT_EV2324/PracticaGIT_EV2324/Form1.Designer.cs
@@ -28,13 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tbText = new System.Windows.Forms.TextBox();
+            this.tbPrice = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbUrgent = new System.Windows.Forms.CheckBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbText
+            // 
+            this.tbText.Location = new System.Drawing.Point(62, 87);
+            this.tbText.Multiline = true;
+            this.tbText.Name = "tbText";
+            this.tbText.Size = new System.Drawing.Size(584, 119);
+            this.tbText.TabIndex = 0;
+            // 
+            // tbPrice
+            // 
+            this.tbPrice.Location = new System.Drawing.Point(125, 304);
+            this.tbPrice.Name = "tbPrice";
+            this.tbPrice.Size = new System.Drawing.Size(139, 26);
+            this.tbPrice.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Texto:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 310);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Coste:";
+            // 
+            // cbUrgent
+            // 
+            this.cbUrgent.AutoSize = true;
+            this.cbUrgent.Location = new System.Drawing.Point(62, 233);
+            this.cbUrgent.Name = "cbUrgent";
+            this.cbUrgent.Size = new System.Drawing.Size(111, 24);
+            this.cbUrgent.TabIndex = 4;
+            this.cbUrgent.Text = "¿Urgente?";
+            this.cbUrgent.UseVisualStyleBackColor = true;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(488, 286);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(158, 44);
+            this.btnCalculate.TabIndex = 5;
+            this.btnCalculate.Text = "Calcular";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.cbUrgent);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbPrice);
+            this.Controls.Add(this.tbText);
+            this.Name = "Form1";
+            this.Text = "PracticaGIT_EV2324";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tbText;
+        private System.Windows.Forms.TextBox tbPrice;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox cbUrgent;
+        private System.Windows.Forms.Button btnCalculate;
     }
 }
 
